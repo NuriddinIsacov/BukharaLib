04d28eae778129bab5ec459ede9992230f6ba601
n.isoqov <isoqov0910@gmail.com>
2025-05-13 10:49:54 +0500

    update:complated celebrities

--- pages/celebrities/[id].vue
+++ pages/celebrities/[id].vue
@@ -1,34 +1,39 @@
 <template>
-  <UContainer class="py-10">
-    <UCard class="p-6 rounded-2xl shadow">
-      <div class="flex flex-col lg:flex-row gap-6">
-        <NuxtImg
-          :src="scholar?.image"
-          alt="scholar.name"
-          class="w-full lg:w-1/3 h-64 object-cover rounded-md"
-        />
-        <div class="flex-1 space-y-4">
-          <h1 class="text-2xl font-bold">{{ scholar?.name }}</h1>
-          <section>
-            <h2 class="text-xl font-semibold">Bolaligi</h2>
-            <p class="text-gray-700">{{ scholar?.childhood }}</p>
-          </section>
-          <section>
-            <h2 class="text-xl font-semibold">Hayoti</h2>
-            <p class="text-gray-700">{{ scholar?.info }}</p>
-          </section>
-          <section>
-            <h2 class="text-xl font-semibold">Faoliyati</h2>
-            <p class="text-gray-700">{{ scholar?.activity }}</p>
-          </section>
-          <section>
-            <h2 class="text-xl font-semibold">Vafoti</h2>
-            <p class="text-gray-700">{{ scholar?.death }}</p>
-          </section>
+  <section
+    id="hero"
+    class="relative w-full bg-[url(/images/celebrities/bg.jpg)] bg-cover bg-no-repeat backdrop-blur-md"
+  >
+    <UContainer class="py-32 ">
+      <UCard class="p-6 rounded-2xl shadow bg-white/80">
+        <div class="flex flex-col lg:flex-row gap-6">
+          <NuxtImg
+            :src="scholar?.image"
+            alt="scholar.name"
+            class="w-full lg:w-1/3 h-64 object-cover rounded-md"
+          />
+          <div class="flex-1 space-y-4">
+            <h1 class="text-2xl font-bold">{{ scholar?.name }}</h1>
+            <section>
+              <h2 class="text-xl font-semibold">Bolaligi</h2>
+              <p class="text-gray-700">{{ scholar?.childhood }}</p>
+            </section>
+            <section>
+              <h2 class="text-xl font-semibold">Hayoti</h2>
+              <p class="text-gray-700">{{ scholar?.info }}</p>
+            </section>
+            <section>
+              <h2 class="text-xl font-semibold">Faoliyati</h2>
+              <p class="text-gray-700">{{ scholar?.activity }}</p>
+            </section>
+            <section>
+              <h2 class="text-xl font-semibold">Vafoti</h2>
+              <p class="text-gray-700">{{ scholar?.death }}</p>
+            </section>
+          </div>
         </div>
-      </div>
-    </UCard>
-  </UContainer>
+      </UCard>
+    </UContainer>
+  </section>
 </template>
 
 <script setup lang="ts">
